4d563853bbc3aa2ac2f43b5678b249243a85b687
Mattia Macor <mtt112358@gmail.com>
2020-02-20 14:30:31 +0100

    1st part done

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,5 +1,7 @@
 class Dose < ApplicationRecord
   validates :description, presence: true
-  belongs_to :cocktails, presence: true, uniqueness: true
-  belongs_to :ingredients, presence: true, uniqueness: true #belongs_to -> singular here?
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true, uniqueness: { scope: :cocktail } # -> read: unique for every cocktail
+  belongs_to :cocktail
+  belongs_to :ingredient  #belongs_to -> singular here?
 end
