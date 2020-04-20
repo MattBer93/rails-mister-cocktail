8e48bba23db44409ca52ecce32f6e2fca6f7ff19
Mattia Macor <mtt112358@gmail.com>
2020-04-20 17:57:56 +0200

    to retest

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
-  resources :cocktails, only: %i[index show] do
+  resources :cocktails, only: %i[index show new create] do
     resources :doses, only: [:create]
     resources :ingredients, only: [:create]
   end
