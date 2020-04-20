618b6c8630e5ff22b885617b4e2a88b851a72b59
Mattia Macor <mtt112358@gmail.com>
2020-04-20 12:43:23 +0200

    bit progress

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,5 +1,6 @@
 class Dose < ApplicationRecord
   validates :ingredient, presence: true, uniqueness: { scope: :cocktail } # -> read: unique for every cocktail
+  validates :description, presence: true
   belongs_to :cocktail
   belongs_to :ingredient  #belongs_to -> singular here?
 end
